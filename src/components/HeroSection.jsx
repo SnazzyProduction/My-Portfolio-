fb684d92264b649cb231772815ff8fdc9bf3da07
SnazzyProduction <stevendshope@gmial.com>
2026-01-05 20:18:13 -0600

    herosection Lets get started change

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -135,7 +135,7 @@ export default function HeroSection() {
                 href="#"
                 className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
               >
-                Get started
+                Lets Get started
               </a>
               <a href="#" className="text-sm/6 font-semibold text-white">
                 Learn more <span aria-hidden="true">→</span>
